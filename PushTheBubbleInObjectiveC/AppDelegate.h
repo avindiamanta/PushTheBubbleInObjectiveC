9ea2e1aeeb82d00fa5b2f4a6f4721760b7f3bdcf
Avindiamanta <avinophotography@gmail.com>
2015-11-18 15:06:58 +0700

    Initial Commit

---
+++ PushTheBubbleInObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushTheBubbleInObjectiveC
+//
+//  Created by Tiffany Diamanta on 11/18/15.
+//  Copyright © 2015 Tiffany Diamanta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushTheBubbleInObjectiveCTests/PushTheBubbleInObjectiveCTests.m
@@ -0,0 +1,39 @@
+//
+//  PushTheBubbleInObjectiveCTests.m
+//  PushTheBubbleInObjectiveCTests
+//
+//  Created by Tiffany Diamanta on 11/18/15.
+//  Copyright © 2015 Tiffany Diamanta. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PushTheBubbleInObjectiveCTests : XCTestCase
+
+@end
+
+@implementation PushTheBubbleInObjectiveCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
